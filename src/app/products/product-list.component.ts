f2740a59781fe613f4d631396779328c5b68c91b
Altana <altanagrishkina@gmail.com>
2023-10-26 17:38:45 +0200

    Working with the Async Pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,35 +1,28 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+
 import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products: Product[] = [];
-  sub!: Subscription;
+  products$: Observable<Product[]> | undefined;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: products => this.products = products,
-        error: err => this.errorMessage = err
-      });
-  }
+    this.products$ = this.productService.getProducts()
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
   }
 
   onAdd(): void {
