f6e1498b1ec03794a83837ae23ec861046a25c42
Altana <altanagrishkina@gmail.com>
2023-11-03 13:14:00 +0100

    Reacting to a Selection: Action Stream

--- src/app/products/product-list-alt/product-list-alt.component.ts
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
 import { EMPTY, Subscription, catchError } from 'rxjs';
 
@@ -7,12 +7,12 @@ import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  selectedProductId = 0;
 
   products: Product[] = [];
   sub!: Subscription;
@@ -25,9 +25,11 @@ export class ProductListAltComponent {
       })
     );
 
+  selectedProduct$ = this.productService.selectedProduct$;
+
   constructor(private productService: ProductService) { }
 
   onSelected(productId: number): void {
-    console.log('Not yet implemented');
+    this.productService.selectedProductChanged(productId);
   }
 }
